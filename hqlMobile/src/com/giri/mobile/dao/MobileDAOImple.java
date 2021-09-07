c64dd806fdab1f0127e7dc2439c972243333716b
girijapj03 <72901278+girijapj03@users.noreply.github.com>
2021-09-07 18:37:23 +0530

    HQL query to update ,delete &get max,min

---
+++ hqlMobile/src/com/giri/mobile/dao/MobileDAOImple.java
@@ -0,0 +1,117 @@
+package com.giri.mobile.dao;
+
+
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+
+import com.giri.mobile.entity.MobileEntity;
+import com.giri.mobile.util.SFUtil;
+
+public class MobileDAOImple implements MobileDAO{
+	private SessionFactory factory = SFUtil.getFactory();
+	Transaction transaction = null;
+
+	@Override
+	public int save(MobileEntity entity) {
+		int key = 0;
+		try (Session session = factory.openSession()) {
+			transaction = session.beginTransaction();
+			key = (int) session.save(entity);
+			session.flush();
+			transaction.commit();
+			return key;
+		}
+	}
+
+	@Override
+	public void updatePriceByColor() {
+		try (Session session = factory.openSession()) {
+			Query query = session
+					.createQuery("update MobileEntity as mobile set mobile.price='23500.0'where mobile.color='Purple'");
+			session.beginTransaction();
+			query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("Updated****");
+		}
+	}
+
+	@Override
+	public void deleteRowById() {
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("delete MobileEntity  mobile where mobile.id=2");
+			session.beginTransaction();
+			query.executeUpdate();
+			session.getTransaction().commit();
+			System.out.println("deleted**");
+		}
+	}
+
+	@Override
+	public void getAllRecords() {
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("from MobileEntity");
+			List<MobileEntity> list = query.list();
+			for (MobileEntity mobileEntity : list) {
+				System.out.println(mobileEntity);
+			}
+		}
+	}
+
+	@Override
+	public double getMobilePriceByBrand(String brand) {
+		double mobilePrice = 0;
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("select mobile.price from MobileEntity mobile where mobile.brand='+brand+'");
+			Object object = query.uniqueResult();
+			if (object != null) {
+				mobilePrice = (double) object;
+			}
+		}
+		return mobilePrice;
+	}
+
+	@Override
+	public double getTotal() {
+		double mobilePrice = 0;
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("select sum(price) from MobileEntity");
+			Object obj = query.uniqueResult();
+			if (obj != null) {
+				mobilePrice = (double) obj;
+			}
+		}
+		return mobilePrice;
+	}
+
+	@Override
+	public double getMaximumPrice() {
+		double mobilePrice = 0;
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("select max(price) from MobileEntity");
+			Object object = query.uniqueResult();
+			if (object != null) {
+				mobilePrice = (double) object;
+			}
+		}
+		return mobilePrice;
+	}
+
+	@Override
+	public double getMinimumPrice() {
+		double mobilePrice = 0;
+		try (Session session = factory.openSession()) {
+			Query query = session.createQuery("select min(price) from MobileEntity");
+			Object obj = query.uniqueResult();
+			if (obj != null) {
+				mobilePrice = (double) obj;
+			}
+		}
+		return mobilePrice;
+	}
+
+}
